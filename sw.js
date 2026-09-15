2c70bc6834c17f0fdc26b4af851790c388eee175
Maurice9323 <3221sjmo@gmail.com>
2026-09-15 17:13:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'the-discipline-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (k) { return k !== CACHE_NAME; })
+            .map(function (k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      var networkFetch = fetch(event.request)
+        .then(function (response) {
+          return caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+        .catch(function () { return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
